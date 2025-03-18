9c1bdaad4a3d0b380715043f27e79ffed1089aa3
nyiyui <+@nyiyui.ca>
2025-03-18 17:33:18 -0400

    fix http client typo

--- polar_data_collector/http_client.py
+++ polar_data_collector/http_client.py
@@ -10,8 +10,8 @@ BASE_URL = os.getenv("BASE_URL", "https://gtxr-vrsa.kiyuri.ca/")
 
 
 def send_data(measurement: HeartRateMeasurement):
-    measure_time = datetime.datetime.now().isoformat()
-    data = {"measure_time": measure_time, "heart_rate": measurement.heart_rate_measurement_value}
+    measured_time = datetime.datetime.now().isoformat()
+    data = {"measured_time": measured_time, "heart_rate": measurement.heart_rate_measurement_value}
     data["rr_intervals"] = " ".join(map(str, measurement.rr_interval)) if measurement.rr_interval else ""
     encode = lambda s: urllib.parse.quote(s, safe="")
     data = "&".join(f"{encode(k)}={encode(str(v))}" for k, v in data.items())
