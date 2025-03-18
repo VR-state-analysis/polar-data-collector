8b82c31b7c046e9e6d74e23718e166b2d71ebe92
nyiyui <+@nyiyui.ca>
2025-03-18 16:55:40 -0400

    use curl (subprocess) instead of urllib
    - urllib wasn't working
    - wanted to extra dependencies (not even requests)
    - curl is already on Raspberry Pi OS

--- polar_data_collector/http_client.py
+++ polar_data_collector/http_client.py
@@ -1,6 +1,6 @@
 import os
-import urllib.request
 import urllib.parse
+import subprocess
 import datetime
 
 from parse import HeartRateMeasurement
@@ -10,9 +10,11 @@ BASE_URL = os.getenv("BASE_URL", "https://gtxr-vrsa.kiyuri.ca/")
 
 
 def send_data(measurement: HeartRateMeasurement):
-    # format is "YYYY-MM-DD HH:MM:SS.SSS"
-    measure_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    measure_time = datetime.datetime.now().isoformat()
     data = {"measure_time": measure_time, "heart_rate": measurement.heart_rate_measurement_value}
     data["rr_intervals"] = " ".join(map(str, measurement.rr_interval)) if measurement.rr_interval else ""
     encode = lambda s: urllib.parse.quote(s, safe="")
-    urllib.request.urlopen(BASE_URL, data=bytes("&".join(f"{encode(k)}={encode(str(v))}" for k, v in data.items()), "utf-8"))
+    data = "&".join(f"{encode(k)}={encode(str(v))}" for k, v in data.items())
+    url = f"{BASE_URL}"
+    cmd = ["curl", "-d", data, url]
+    subprocess.run(cmd, capture_output=True, check=True)
